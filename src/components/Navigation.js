53cd30f8fd91c039dd6d62d18974107e237943e3
Adriana Heesh <adrianaheesh@gmail.com>
2021-10-19 13:50:58 +1000

    created navigation bar to link to all components using react router dom

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,52 @@
 import React from 'react'
+import Home from './Home'
+import About from './About'
+import Projects from './Projects'
+import Contact from './Contact'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
 
 const Navigation = () => {
     return (
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Connect</li>
-            </ul>
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/projects">Projects</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <>
+        <h3>This is where the projects go</h3>
+        </>
+    )
+}
+
+export default Projects
